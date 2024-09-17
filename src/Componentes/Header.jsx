098ac83a895fca534986032978c2e5e71d8dbd79
Giovanna França <giovanna.gfranca@sptech.school>
2024-09-17 20:26:23 -0300

    Concluido a header e iniciando a parte de descrubrir fornecedores

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-<link rel="stylesheet" href="Header.css" />
+import styles from '../css/Header.module.css'
+
 
 function Header() {
   return (
-    <header className="header">
-      <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/9600642490050d671517648081bc097cfb811321c5077f7d767705bc28cd6a8e?placeholderIfAbsent=true&apiKey=ba428365f10b481897249f94cd5b5af7" alt="Bridee header image" className="header-image" />
+    <header className = {styles.Header}>
+      <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/9600642490050d671517648081bc097cfb811321c5077f7d767705bc28cd6a8e?placeholderIfAbsent=true&apiKey=ba428365f10b481897249f94cd5b5af7" alt="Bridee header image" className={"styles.header-image"} />
       
     </header>
   );
