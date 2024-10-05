05c2a32c07733d0863ea0a311b6bb5589bbded5c
Amanda Geovanna <amanda.sousa@sptech.school>
2024-10-05 01:58:58 -0300

    refatorando código com map para ficar mais limpo

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -10,7 +10,15 @@ import "../../index.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
-import { faUser, faListCheck, faUsers, faClipboardList, faChair, faCalculator, faChevronRight  } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faListCheck,
+  faUsers,
+  faClipboardList,
+  faChair,
+  faCalculator,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 import MenuLink from "../MenuLink/MenuLink";
 
 function NavComp() {
@@ -19,6 +27,34 @@ function NavComp() {
   const handleClick = (selecao) => {
     setSelecaoAtual(selecao);
   };
+
+  const ferramentasDePlanejamento = [
+    {
+      nome: "Painel",
+      descricao: "Gerencie cada detalhe do seu casamento com facilidade, com todas as ferramentas que você precisa em um só lugar.",
+      icon: faClipboardList,
+    },
+    {
+      nome: "Lista de convidados",
+      descricao: "Uma lista de convidados prática e intuitiva para ajudar você a gerenciar facilmente quem irá convidar e acompanhar as confirmações.",
+      icon: faClipboardList,
+    },
+    {
+      nome: "Lista de tarefas",
+      descricao: "Nossa lista de verificação de planejamento de casamento é a maneira mais fácil de gerenciar o cronograma do planejamento do seu casamento sem estresse",
+      icon: faListCheck,
+    },
+    {
+      nome: "Planejador de assentos",
+      descricao: "Um planejador de assentos intuitivo para organizar sua recepção de forma fácil e eficaz.",
+      icon: faChair,
+    },
+    {
+      nome: "Calculadora Financeira",
+      descricao: "Uma calculadora financeira prática para ajudá-la a planejar o orçamento do seu grande dia.",
+      icon: faCalculator,
+    },
+  ];
 
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
@@ -46,149 +82,22 @@ function NavComp() {
                 id="ferramentas-planejamento"
                 className="menu_suspenso"
               >
-                <Container className="container-menu-suspenso">
-                  <Container className="container_one">
-                    <div className="titulo">
-                      <div className="icon"></div>
-                      <NavDropdown.Item
-                        onClick={() => handleClick("Painel")}
-                        className={
-                          selecaoAtual === "Painel" ? "selecao-ativa" : ""
-                        }
-                      >
-                        Painel
-                      </NavDropdown.Item>
-                    </div>
+                 {ferramentasDePlanejamento.map((item) => (
+                  <Container key={item.nome} className="container-item">
+                    <NavDropdown.Item
+                      onClick={() => handleClick(item.nome)}
+                      className={selecaoAtual === item.nome ? "selecao-ativa" : ""}
+                    >
+                      <FontAwesomeIcon icon={item.icon} className="icons" />
+                      {item.nome}
+                      <FontAwesomeIcon icon={faChevronRight} className="icons_arrow" />
+                    </NavDropdown.Item>
                     <div className="descricao">
-                      <span>
-                        Gerencie cada detalhe do seu casamento com facilidade,
-                        com todas as ferramentas que você precisa em um só
-                        lugar.
-                      </span>
+                      <span>{item.descricao}</span>
                     </div>
+                    <NavDropdown.Divider />
                   </Container>
-
-                  <NavDropdown.Divider />
-
-                  <Container className="container_two">
-                    <div className="coluna_one">
-                      <div className="titulo">
-                        
-                        <NavDropdown.Item
-                          onClick={() => handleClick("Lista de convidados")}
-                          className={
-                            selecaoAtual === "Lista de convidados"
-                              ? "selecao-ativa"
-                              : ""
-                          }
-                        >
-                          <FontAwesomeIcon icon={faClipboardList} className="icons" />
-                          Lista de convidados
-                          <FontAwesomeIcon icon={faChevronRight} className="icons_arrow" /> 
-                        </NavDropdown.Item>
-                      </div>
-
-                      <div className="descricao">
-                        <span>
-                          Uma lista de convidados prática e intuitiva para
-                          ajudar você a gerenciar facilmente quem irá convidar e
-                          acompanhar as confirmações.
-                        </span>
-                      </div>
-                    </div>
-
-                    <div className="coluna_two">
-                      <div className="titulo">
-                        <div className="icon"></div>
-                        <NavDropdown.Item
-                          onClick={() => handleClick("Lista de tarefas")}
-                          className={
-                            selecaoAtual === "Lista de tarefas"
-                              ? "selecao-ativa"
-                              : ""
-                          }
-                        >
-                           <FontAwesomeIcon icon={faListCheck} className="icons" />
-                          Lista de tarefas
-                          <FontAwesomeIcon icon={faChevronRight} className="icons_arrow" /> 
-
-                        </NavDropdown.Item>
-                        <div className="arrow">
-
-                        </div>
-                      </div>
-
-                      <div className="descricao">
-                        <span>
-                          Nossa lista de verificação de planejamento de
-                          casamento é a maneira mais fácil de gerenciar o
-                          cronograma do planejamento do seu casamento sem
-                          estresse.
-                        </span>
-                      </div>
-                    </div>
-                  </Container>
-
-                  <NavDropdown.Divider />
-
-                  <Container className="container_three">
-                    <div className="coluna_three">
-                      <div className="titulo">
-                        <div className="icon"></div>
-                        <NavDropdown.Item
-                          onClick={() => handleClick("Planejador de assentos")}
-                          className={
-                            selecaoAtual === "Planejador de assentos"
-                              ? "selecao-ativa"
-                              : ""
-                          }
-                        >
-                        <FontAwesomeIcon icon={faChair} className="icons" />
-
-                          Planejador de assentos
-                          <FontAwesomeIcon icon={faChevronRight} className="icons_arrow" /> 
-
-                        </NavDropdown.Item>
-                      </div>
-                      
-                      <div className="descricao">
-                        <span>
-                          Nossa lista de verificação de planejamento de
-                          casamento é a maneira mais fácil de gerenciar o
-                          cronograma do planejamento do seu casamento sem
-                          estresse.
-                        </span>
-                      </div>
-                    </div>
-
-                    <div className="coluna_four">
-                      <div className="titulo">
-                        <div className="icon"></div>
-                        <NavDropdown.Item
-                          onClick={() => handleClick("Calculadora Financeira")}
-                          className={
-                            selecaoAtual === "Calculadora Financeira"
-                              ? "selecao-ativa"
-                              : ""
-                          }
-                        >
-                          <FontAwesomeIcon icon={faCalculator} className="icons" />
-                          Calculadora Financeira
-                          <FontAwesomeIcon icon={faChevronRight} className="icons_arrow" /> 
-
-                        </NavDropdown.Item>
-                      </div>
-                      <div className="descricao">
-                        <span>
-                          Nossa lista de verificação de planejamento de
-                          casamento é a maneira mais fácil de gerenciar o
-                          cronograma do planejamento do seu casamento sem
-                          estresse.
-                        </span>
-                      </div>
-                    </div>
-                  </Container>
-                </Container>
+                ))}
               </NavDropdown>
 
               {/* Locais e Fornecedores */}
@@ -205,21 +114,59 @@ function NavComp() {
                 id="locais-fornecedores"
                 className="menu-suspenso"
               >
-                <NavDropdown.Item href="#action/3.1">Locais</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Fornecedores
-                </NavDropdown.Item>
+                <Container className="container-menu-suspenso">
+                  <Container className="container_one_fornecedores">
+                    <div className="titulo">
+                      <div className="icon"></div>
+                      <NavDropdown.Item
+                        onClick={() => handleClick("Painel")}
+                        className={
+                          selecaoAtual === "Painel" ? "selecao-ativa" : ""
+                        }
+                      >
+                        Por categoria
+                      </NavDropdown.Item>
+                    </div>
+                    
+                      <div className="lista">
+                        <span>Fotógrafo</span>
+                        <span>Florista</span>
+                      </div>
+                    
+                  </Container>
+                </Container>
+
+                <NavDropdown.Divider />
+
+                <Container className="container_two_fornecedores">
+                  <div className="coluna_one">
+                    <div className="titulo">
+                      <NavDropdown.Item
+                        onClick={() => handleClick("Lista de convidados")}
+                        className={
+                          selecaoAtual === "Lista de convidados"
+                            ? "selecao-ativa"
+                            : ""
+                        }
+                      >
+                        <FontAwesomeIcon
+                          icon={faClipboardList}
+                          className="icons"
+                        />
+                        Estilo de casamento
+                        <FontAwesomeIcon
+                          icon={faChevronRight}
+                          className="icons_arrow"
+                        />
+                      </NavDropdown.Item>
+                    </div>
+                  </div>
+                </Container>
               </NavDropdown>
             </Nav>
 
             {/* Ícones na direita */}
             <Nav className="icones-direita">
-              {/* <Nav.Link href="#busca" className="icone-busca">
-                  <FontAwesomeIcon
-                    icon={faSearch}
-                    style={{ color: "#FFFFFF" }}
-                  />
-                </Nav.Link> */}
               <Nav.Link href="#usuario" className="icone-usuario">
                 <FontAwesomeIcon icon={faUser} style={{ color: "#FFFFFF" }} />
               </Nav.Link>
