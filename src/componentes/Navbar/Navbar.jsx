564f82b6f4e67353691b098fc68a44a5b201dec0
Amanda Geovanna <amanda.sousa@sptech.school>
2024-10-05 11:38:50 -0300

    slice no map para dividir em colunas

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -31,27 +31,32 @@ function NavComp() {
   const ferramentasDePlanejamento = [
     {
       nome: "Painel",
-      descricao: "Gerencie cada detalhe do seu casamento com facilidade, com todas as ferramentas que você precisa em um só lugar.",
+      descricao:
+        "Gerencie cada detalhe do seu casamento com facilidade, com todas as ferramentas que você precisa em um só lugar.",
       icon: faClipboardList,
     },
     {
       nome: "Lista de convidados",
-      descricao: "Uma lista de convidados prática e intuitiva para ajudar você a gerenciar facilmente quem irá convidar e acompanhar as confirmações.",
+      descricao:
+        "Uma lista de convidados prática e intuitiva para ajudar você a gerenciar facilmente quem irá convidar e acompanhar as confirmações.",
       icon: faClipboardList,
     },
     {
       nome: "Lista de tarefas",
-      descricao: "Nossa lista de verificação de planejamento de casamento é a maneira mais fácil de gerenciar o cronograma do planejamento do seu casamento sem estresse",
+      descricao:
+        "Nossa lista de verificação de planejamento de casamento é a maneira mais fácil de gerenciar o cronograma do planejamento do seu casamento sem estresse",
       icon: faListCheck,
     },
     {
       nome: "Planejador de assentos",
-      descricao: "Um planejador de assentos intuitivo para organizar sua recepção de forma fácil e eficaz.",
+      descricao:
+        "Um planejador de assentos intuitivo para organizar sua recepção de forma fácil e eficaz.",
       icon: faChair,
     },
     {
       nome: "Calculadora Financeira",
-      descricao: "Uma calculadora financeira prática para ajudá-la a planejar o orçamento do seu grande dia.",
+      descricao:
+        "Uma calculadora financeira prática para ajudá-la a planejar o orçamento do seu grande dia.",
       icon: faCalculator,
     },
   ];
@@ -82,22 +87,55 @@ function NavComp() {
                 id="ferramentas-planejamento"
                 className="menu_suspenso"
               >
-                 {ferramentasDePlanejamento.map((item) => (
-                  <Container key={item.nome} className="container-item">
-                    <NavDropdown.Item
-                      onClick={() => handleClick(item.nome)}
-                      className={selecaoAtual === item.nome ? "selecao-ativa" : ""}
-                    >
-                      <FontAwesomeIcon icon={item.icon} className="icons" />
-                      {item.nome}
-                      <FontAwesomeIcon icon={faChevronRight} className="icons_arrow" />
-                    </NavDropdown.Item>
-                    <div className="descricao">
-                      <span>{item.descricao}</span>
-                    </div>
-                    <NavDropdown.Divider />
-                  </Container>
-                ))}
+                <Container className="container-pai">
+                  <div className="coluna">
+                    {ferramentasDePlanejamento.slice(0, 1).map((item) => (
+                      <Container key={item.nome} className="container-item">
+                        <NavDropdown.Item
+                          onClick={() => handleClick(item.nome)}
+                        >
+                          <FontAwesomeIcon icon={item.icon} className="icons" />
+                          <span>{item.nome}</span>
+                        </NavDropdown.Item>
+                        <div className="descricao">
+                          <span>{item.descricao}</span>
+                        </div>
+                      </Container>
+                    ))}
+                  </div>
+
+                  <div className="coluna">
+                    {ferramentasDePlanejamento.slice(1, 3).map((item) => (
+                      <Container key={item.nome} className="container-item">
+                        <NavDropdown.Item
+                          onClick={() => handleClick(item.nome)}
+                        >
+                          <FontAwesomeIcon icon={item.icon} className="icons" />
+                          <span>{item.nome}</span>
+                        </NavDropdown.Item>
+                        <div className="descricao">
+                          <span>{item.descricao}</span>
+                        </div>
+                      </Container>
+                    ))}
+                  </div>
+
+                  <div className="coluna">
+                    {ferramentasDePlanejamento.slice(3, 5).map((item) => (
+                      <Container key={item.nome} className="container-item">
+                        <NavDropdown.Item
+                          onClick={() => handleClick(item.nome)}
+                        >
+                          <FontAwesomeIcon icon={item.icon} className="icons" />
+                          <span>{item.nome}</span>
+                        </NavDropdown.Item>
+                        <div className="descricao">
+                          <span>{item.descricao}</span>
+                        </div>
+                      </Container>
+                    ))}
+                  </div>
+                </Container>
               </NavDropdown>
 
               {/* Locais e Fornecedores */}
@@ -112,9 +150,9 @@ function NavComp() {
                   </>
                 }
                 id="locais-fornecedores"
-                className="menu-suspenso"
+                className="menu_suspenso"
               >
-                <Container className="container-menu-suspenso">
+                <Container className="container_suspenso">
                   <Container className="container_one_fornecedores">
                     <div className="titulo">
                       <div className="icon"></div>
@@ -127,12 +165,11 @@ function NavComp() {
                         Por categoria
                       </NavDropdown.Item>
                     </div>
-                    
-                      <div className="lista">
-                        <span>Fotógrafo</span>
-                        <span>Florista</span>
-                      </div>
-                    
+
+                    <div className="lista">
+                      <span>Fotógrafo</span>
+                      <span>Florista</span>
+                    </div>
                   </Container>
                 </Container>
 
