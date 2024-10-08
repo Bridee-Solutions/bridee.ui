672eca831728dd2f92904e1669b247c019690e86
beatriz <beatriz.rocha@sptech.school>
2024-10-08 20:50:56 -0300

    configuração visual inicial da tela

--- src/pages/ListaTarefas/ListaTarefas.jsx
+++ src/pages/ListaTarefas/ListaTarefas.jsx
@@ -1,8 +1,21 @@
+import styles from './ListaTarefas.module.css';
 import Navbar from "../../componentes/Navbar/Navbar";
 
 function ListaTarefas() { 
     return (
-       <Navbar/>
+        <div className={styles['lista-de-tarefas']}>
+            <Navbar/>
+            <div className= {styles.conteudo}>
+                <div className= {styles.tituloPage}>
+                    <p>Ferramentas de planejamento {'>'} <b>Lista de tarefas</b></p>
+                </div>
+                <div className={styles['card-fundo']}>
+                    <div className={styles.titulo}>
+                        <h6>Lista de Tarefas</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
