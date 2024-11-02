2b5d5ef58db6bcbe0168f1b81649c438834ae45a
Amanda Geovanna <amanda.sousa@sptech.school>
2024-11-02 00:45:56 -0300

    Update package.json dependencies and add new SVG file
    - Add uuid dependency to package.json
    - Add new SVG file for calculadora feature
    
    Refactor CategoriaCalculadora.module.css
    
    - Refactor CSS styles in CategoriaCalculadora.module.css
    
    Refactor Calculadora.module.css
    
    - Refactor CSS styles in Calculadora.module.css

--- src/componentes/CategoriaCalculadora/CategoriaCalculadora.jsx
+++ src/componentes/CategoriaCalculadora/CategoriaCalculadora.jsx
@@ -1,13 +1,16 @@
 import styles from "../../pages/Calculadora/Calculadora.module.css";
-import stylesCalc from "../CategoriaCalculadora/CategoriaCalculadora.module.css"
+import stylesCalc from "../CategoriaCalculadora/CategoriaCalculadora.module.css";
 import "./CategoriaCalculadora.module.css";
 import "../../index.css";
 import add from "../../assets/calculadora/add.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+
 import {
   faChevronDown,
   faChevronUp,
   faTrash,
+  faCheck
 } from "@fortawesome/free-solid-svg-icons";
 
 function CategoriaCalc({
@@ -21,12 +24,31 @@ function CategoriaCalc({
   adicionarItem,
   removerItem,
 }) {
+  // Estado para controlar se houve modificação
+  const [isModified, setIsModified] = useState(false);
+
+  // Função para verificar alterações nos campos de entrada
+  const handleModifiedChange = (e, catIndex, itemIndex) => {
+    handleInputChange(e, catIndex, itemIndex);
+    setIsModified(true); // Define como modificado quando a função é chamada
+  };
+
+  const handleTituloModifiedChange = (e, catIndex, itemIndex) => {
+    handleTituloChange(e, catIndex, itemIndex);
+    setIsModified(true);
+  };
+
+  // Define o estado como `false` ao salvar as alterações
+  const salvarAlteracoes = () => {
+    // Aqui você pode adicionar lógica para salvar as alterações se necessário
+    setIsModified(false); // Reseta o estado após salvar
+  };
   return (
-    <div className={styles.categoria}>
-      <div className={styles.cabecalhoCategoria}>
-        <div className={styles.containerCat}>
+    <div className={stylesCalc.categoria}>
+      <div className={stylesCalc.cabecalhoCategoria}>
+        <div className={stylesCalc.containerCat}>
           <button
-            className={styles.setaCategoria}
+            className={stylesCalc.setaCategoria}
             onClick={() => toggleCategoria(catIndex)}
           >
             {categoria.aberta ? (
@@ -36,40 +58,44 @@ function CategoriaCalc({
             )}
           </button>
 
-          <div className={styles.iconeCategoria}>
+          <div className={stylesCalc.iconeCategoria}>
             <img src={categoria.icon} alt="Ícone da Categoria" />
           </div>
 
-          <div className={styles.categoriaNome}>{categoria.nome}</div>
+          <div className={stylesCalc.categoriaNome}>{categoria.nome}</div>
         </div>
       </div>
 
       {categoria.aberta && (
         <>
           {categoria.itens.map((item, itemIndex) => (
-            <div key={`item-${itemIndex}`} className={styles.item}>
-              <div className={styles.containerItem}>
-                <div className={styles.itemEsquerda}>
+            <div key={`item-${itemIndex}`} className={stylesCalc.item}>
+              <div className={stylesCalc.containerItem}>
+                <div className={stylesCalc.itemEsquerda}>
                   {itemEditando.catIndex === catIndex &&
                   itemEditando.itemIndex === itemIndex ? (
                     // Campo de texto para edição do título
                     <input
                       type="text"
+                      maxLength={42}
                       value={item.titulo}
                       onChange={(e) =>
-                        handleTituloChange(e, catIndex, itemIndex)
+                        handleTituloModifiedChange(e, catIndex, itemIndex)
                       }
                       onBlur={() =>
                         setItemEditando({ catIndex: null, itemIndex: null })
-                        
                       } // Sai do modo de edição ao desfocar
-                      className={`${styles.inputTitulo} ${stylesCalc.titulo}`}                    />
+                      className={`${stylesCalc.inputTitulo} ${stylesCalc.titulo}`}
+                    />
                   ) : (
                     // Exibe o título e habilita o modo de edição ao clicar
                     <span
                       onClick={() => setItemEditando({ catIndex, itemIndex })}
                       className={`${stylesCalc.titulo} ${
-                        itemEditando.catIndex === catIndex && itemEditando.itemIndex === itemIndex ? stylesCalc.editando : ""
+                        itemEditando.catIndex === catIndex &&
+                        itemEditando.itemIndex === itemIndex
+                          ? stylesCalc.editando
+                          : ""
                       }`}
                     >
                       {item.titulo}
@@ -77,38 +103,55 @@ function CategoriaCalc({
                   )}
                 </div>
 
-                <div className={styles.inputBox}>
-                  <span>R$</span>
-                  {itemEditando.catIndex === catIndex &&
-                  itemEditando.itemIndex === itemIndex &&
-                  itemEditando.field === "custo" ? (
-                    <input
-                    className={`${styles.inputBox} ${styles.inputCusto}`}                      value={item.custo}
-                      onChange={(e) =>
-                        handleInputChange(e, catIndex, itemIndex)
-                      }
-                      onBlur={() =>
-                        setItemEditando({
-                          catIndex: null,
-                          itemIndex: null,
-                          field: null,
-                        })
-                      }
-                    />
-                  ) : (
-                    <span
-                      onClick={() =>
-                        setItemEditando({ catIndex, itemIndex, field: "custo" })
-                      } 
-                      
-                    >
-                      {item.custo}
-                    </span>
-                  )}
-
+                <div className={stylesCalc.inputBox}>
+                  <div className={stylesCalc.containerDinheiro}>
+                    <span>R$</span>
+                    {itemEditando.catIndex === catIndex &&
+                    itemEditando.itemIndex === itemIndex &&
+                    itemEditando.field === "custo" ? (
+                      <input
+                        className={`${stylesCalc.inputBoxEdit} ${stylesCalc.inputCusto}`}
+                        value={item.custo}
+                        maxLength={8}
+                        type="number"
+                        onChange={(e) =>
+                          handleModifiedChange(e, catIndex, itemIndex)
+                        }
+                        onKeyDown={(e) => {
+                          if (e.key === "Enter") {
+                            // Sai do estado de edição
+                            setItemEditando({
+                              catIndex: null,
+                              itemIndex: null,
+                              field: null,
+                            });
+                          }
+                        }}
+                        onBlur={() =>
+                          setItemEditando({
+                            catIndex: null,
+                            itemIndex: null,
+                            field: null,
+                          })
+                        }
+                      />
+                    ) : (
+                      <span
+                        onClick={() =>
+                          setItemEditando({
+                            catIndex,
+                            itemIndex,
+                            field: "custo",
+                          })
+                        }
+                      >
+                        {item.custo}
+                      </span>
+                    )}
+                  </div>
                   <FontAwesomeIcon
                     icon={faTrash}
-                    className={styles.iconeLixeira}
+                    className={stylesCalc.iconeLixeira}
                     onClick={() => removerItem(catIndex, itemIndex)}
                   />
                 </div>
@@ -117,7 +160,7 @@ function CategoriaCalc({
           ))}
 
           <div
-            className={styles.adicionarItem}
+            className={stylesCalc.adicionarItem}
             onClick={() => adicionarItem(catIndex)}
           >
             <img src={add} alt="Adicionar item" />
@@ -125,6 +168,21 @@ function CategoriaCalc({
           </div>
         </>
       )}
+
+      <div className={stylesCalc.salvar}>
+        {isModified && (
+          <button
+            className={stylesCalc.buttonSaveChanges}
+            onClick={salvarAlteracoes}
+          >
+                    <FontAwesomeIcon
+                      icon={faCheck}
+                      className={styles.iconExport}
+                    />{" "}
+            Salvar alterações
+          </button>
+        )}
+      </div>
     </div>
   );
 }
--- src/pages/Calculadora/Calculadora.jsx
+++ src/pages/Calculadora/Calculadora.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import Navbar from "../../componentes/Navbar/Navbar";
 import styles from "./Calculadora.module.css";
 import "../../index.css";
@@ -9,10 +9,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import decoracao from "../../assets/calculadora/decoracaocalculadora.svg";
 import moda from "../../assets/calculadora/modabeleza.svg";
 import aliancas from "../../assets/calculadora/aliancas.svg";
+import etiqueta from "../../assets/calculadora/etiqueta.svg";
 import transporte from "../../assets/calculadora/transporteacomodacao.svg";
 import entretenimento from "../../assets/calculadora/entretenimentocalc.svg";
 import CategoriaCalc from "../../componentes/CategoriaCalculadora/CategoriaCalculadora";
-
 
 import LinkButton from "../../componentes/LinkButton/LinkButton";
 import Baseboard from "../../componentes/LandingPage/BaseBoard/Baseboard";
@@ -23,49 +23,51 @@ import ModalFooter from "../../componentes/Modal/ModalFooter/ModalFooter";
 import ModalFooterButton from "../../componentes/Modal/ModalFooterButton/ModalFooterButton";
 import ArcoFinanceiro from "../../componentes/ArcoFinanceiro/ArcoFinanceiro";
 
+import { v4 as uuidv4 } from "uuid";
+
 function Calculadora() {
   const [categorias, setCategorias] = useState([
     {
-      id: `cat-${new Date().getTime()}-1`,
+      id: uuidv4(),
       nome: "Moda e beleza",
       icon: moda,
       itens: [{ titulo: "Vestido de Daminha de Honra", custo: 400 }],
       aberta: true,
     },
     {
-      id: `cat-${new Date().getTime()}-1`,
+      id: uuidv4(),
       nome: "Alianças de casamento",
       icon: aliancas,
       itens: [{ titulo: "Meu anel", custo: 2000 }],
       aberta: true,
     },
     {
-      id: `cat-${new Date().getTime()}-1`,
+      id: uuidv4(),
       nome: "Decoração",
       icon: decoracao,
       itens: [{ titulo: "Teste", custo: 2000 }],
       aberta: true,
     },
     {
-      id: `cat-${new Date().getTime()}-1`,
+      id: uuidv4(),
       nome: "Transporte e Acomodação",
       icon: transporte,
       itens: [{ titulo: "Teste", custo: 2000 }],
       aberta: true,
     },
     {
-      id: `cat-${new Date().getTime()}-1`,
+      id: uuidv4(),
       nome: "Entretenimento",
       icon: entretenimento,
       itens: [{ titulo: "Teste", custo: 2000 }],
       aberta: true,
     },
   ]);
-  
+
   const [isModalOpen, setIsModalOpen] = useState(false); // controle do modal
   const [gasto, setGasto] = useState(0); // valor inicial de gasto
   const [total] = useState(10000); // valor total fixo p exemplo
-
+  const [totalAtual, setTotalAtual] = useState(0);
   // função para abrir o modal
   const handleEditClick = () => {
     setIsModalOpen(true);
@@ -95,7 +97,7 @@ function Calculadora() {
   const adicionarItem = (catIndex) => {
     const novasCategorias = [...categorias];
     novasCategorias[catIndex].itens.push({
-      id: `item-${new Date().getTime()}`, 
+      id: `item-${new Date().getTime()}`,
       titulo: "Novo item",
       custo: 0,
     });
@@ -105,7 +107,7 @@ function Calculadora() {
   // Função para remover um item
   function removerItem(catIndex, itemIndex) {
     const novaCategoria = [...categorias];
-    novaCategoria[catIndex].itens.splice(itemIndex, 1); 
+    novaCategoria[catIndex].itens.splice(itemIndex, 1);
     setCategorias(novaCategoria);
   }
 
@@ -115,13 +117,42 @@ function Calculadora() {
     itemIndex: null,
     field: null,
   });
- 
+
   const handleTituloChange = (e, catIndex, itemIndex) => {
     const { value } = e.target;
     const novasCategorias = [...categorias];
     novasCategorias[catIndex].itens[itemIndex].titulo = value;
     setCategorias(novasCategorias);
   };
+
+  // como o nome já diz...
+  const adicionarCategoria = () => {
+    console.log("Adicionando nova categoria");
+
+    const novaCategoria = {
+      id: uuidv4(),
+      nome: "Nova Categoria",
+      icon: etiqueta,
+      itens: [{ titulo: "Novo item", custo: 0 }],
+      aberta: true,
+    };
+
+    setCategorias([...categorias, novaCategoria]);
+  };
+
+  useEffect(() => {
+    const somaTotal = categorias.reduce((acc, categoria) => {
+      return (
+        acc +
+        categoria.itens.reduce((somaItens, item) => {
+          return somaItens + (parseFloat(item.custo) || 0); // Somando os custos dos itens, lidando com valores não numéricos
+        }, 0)
+      );
+    }, 0);
+
+    setTotalAtual(somaTotal); // Atualiza o total com a soma de todas as categorias
+  }, [categorias]);
+
 
   return (
     <div className={styles.background}>
@@ -138,10 +169,7 @@ function Calculadora() {
                 Calculadora Financeira
               </span>
               <span className={styles.descricao}>
-                Organize os gastos do seu evento por categorias, adicionando
-                itens e valores. Veja o total atualizado e exporte suas
-                informações a qualquer momento.{" "}
-              </span>
+              Organize gastos por categoria, acompanhe o total e exporte dados facilmente.              </span>
             </div>
             <div>
               <button className={styles.buttonExport}>
@@ -179,15 +207,22 @@ function Calculadora() {
             <div className={styles.containerInferior}>
               <div className={styles.box}>
                 <div className={styles.containerEsquerda}>
-                  <div className={styles.containerAdcCategoria}>
+                  <div
+                    className={styles.containerAdcCategoria}
+                    onClick={adicionarCategoria}
+                  >
                     <LinkButton label="Adicionar nova categoria" />
                   </div>
                 </div>
+
                 <div className={styles.containerDireita}>
+                 
                   <div>
                     <div className={styles.containerTotal}>
                       <span className={styles.total}>Total atual</span>
-                      <span className={styles.valor}>R$500.00</span>
+                      <span className={styles.valor}>
+                        R${totalAtual.toFixed(2)}
+                      </span>
                     </div>
                     <div className={styles.orcamentoContainer}>
                       <span className={styles.orcamento}>Orçamento:</span>
