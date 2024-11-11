92606b8f06abb78a4022a99164d0e8348f6f7c40
Amanda Geovanna <amanda.sousa@sptech.school>
2024-11-11 20:12:07 -0300

    refatorando todo código

--- src/componentes/CategoriaNavegacao/CategoriaNavegacao.jsx
+++ src/componentes/CategoriaNavegacao/CategoriaNavegacao.jsx
@@ -1,13 +1,22 @@
 import styles from "./CategoriaNavegacao.module.css";
+import CardNavegacao from "../PesquisaCategoria/CardNavegacao";
+function CategoriaNavegacao({ categorias, onSelectCategory, tipo }) {
+  return (
+    <div className={styles.navegarCategoria}>
+      <div className={styles.containerTitulo}>
+        <span className={styles.titulo}>
+        {tipo === "fornecedores" ? "Navegar por categoria" : tipo === "locais" ? "Navegar por estilo" : ""}
 
-function CategoriaNavegacao({ categorias, onSelectCategory }) {
-    return (
-      <div className={styles.navegarCategoria}>
-        <div className={styles.containerTitulo}>
-          <span className={styles.titulo}>Navegar por estilo</span>
-          <span className={styles.subtitulo}>Descrição opcional...</span>
-        </div>
-        <div className={styles.containerCards}>
+        </span>
+        <span className={styles.subtitulo}>
+          {tipo === "fornecedores"
+            ? "Explore os melhores fornecedores de casamento em todas as categorias e descubra o melhor dos melhores para o seu dia especial."
+            : tipo === "locais"
+            ? "Locais que combinam com seu estilo e deixam o momento ainda mais especial" : ""}
+        </span>
+      </div>
+      <div className={styles.containerCards}>
+        <div className={styles.cadaCardLocal}>
           {categorias.map((categoria, index) => (
             <CardNavegacao
               key={index}
@@ -18,5 +27,8 @@ function CategoriaNavegacao({ categorias, onSelectCategory }) {
           ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+export default CategoriaNavegacao;
