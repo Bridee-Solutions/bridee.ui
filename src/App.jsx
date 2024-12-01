0ec0b47778d9f527cdd3b830e43b57cc57e34410
Ian Silva Santos <ian.santos@sptech.school>
2024-12-01 11:00:46 -0300

    Iniciando a criação do gerenciador de estado

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,26 @@ import ListaConvidados from './pages/ListaConvidados/ListaConvidados';
 import PlanejadorAssentos from './pages/PlanejadorAssentos/PlanejadorAssentos';
 import ListaTarefas from './pages/ListaTarefas/ListaTarefas';
 import Convites from './pages/Convites/Convites';
+import { CasalContextProvider } from './context/CasalContext';
 
 function App() {
 
   return (
    <Router>
-    <Routes>
-      <Route path='/' element={<LandingPage/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/tela-inicial' element={<TelaInicial/>}></Route>
-      <Route path='/painel' element={<Painel/>}></Route>
-      <Route path='/calculadora-financeira' element={<Calculadora/>}></Route>
-      <Route path='/lista-convidados' element={<ListaConvidados/>}></Route>
-      <Route path='/planejador-assentos' element={<PlanejadorAssentos/>}></Route>
-      <Route path='/lista-tarefas' element={<ListaTarefas/>}></Route>
-      <Route path='/convites/:casamentoId' element={<Convites/>}></Route>
-      {/* <Route path='*' element={<Error/>}></Route>       */}
-    </Routes>
+    <CasalContextProvider>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/tela-inicial' element={<TelaInicial/>}></Route>
+          <Route path='/painel' element={<Painel/>}></Route>
+          <Route path='/calculadora-financeira' element={<Calculadora/>}></Route>
+          <Route path='/lista-convidados' element={<ListaConvidados/>}></Route>
+          <Route path='/planejador-assentos' element={<PlanejadorAssentos/>}></Route>
+          <Route path='/lista-tarefas' element={<ListaTarefas/>}></Route>
+          <Route path='/convites/:casamentoId' element={<Convites/>}></Route>
+        <Route path='*' element={<Error/>}></Route>      
+      </Routes>
+    </CasalContextProvider>
    </Router>
   )
 }
