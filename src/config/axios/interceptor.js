cfa6f4e883e2c1f17860ea5048f32f9300821ca2
Ian Silva Santos <ian.santos@sptech.school>
2024-12-03 23:01:59 -0300

    Configurando lógica do axios interceptor response e request do refresh token

--- src/config/axios/interceptor.js
+++ src/config/axios/interceptor.js
@@ -1,3 +1,4 @@
+import { encrypt } from "../../utils/criptografia";
 import { Api } from "./axios";
 
 const rotasExcluidas = ["/login", "/", "/cadastrar", "/reenviar-email"]
@@ -12,3 +13,30 @@ Api.interceptors.request.use(config => {
       }
       return config;
 })
+
+Api.interceptors.response.use((response) => {
+    return response
+    }, (error) => {
+        const config = error.config
+
+        if(!rotasExcluidas.includes(config.url) && error.response){
+            if(error.response.status == 401 && !config._retry){
+                config._retry = true
+                try{
+                    refreshToken().catch((error) => {
+                        if(error.status == 401){
+                            localStorage.clear();
+                        }
+                    })
+                }catch(err){
+                    return Promise.reject(err)
+                }
+            }
+        }
+        return Promise.reject(error);
+    }
+)
+
+const refreshToken = async () => {
+    return await Api.post(`/refresh-token`);
+}
