5a2f04a13e1fd0673e3aa0dfd87a0c2325d1f51d
Amanda Geovanna <amanda.sousa@sptech.school>
2024-12-07 00:36:20 -0300

    em breve aviso pagina planejador

--- src/pages/PlanejadorAssentos/PlanejadorAssentos.jsx
+++ src/pages/PlanejadorAssentos/PlanejadorAssentos.jsx
@@ -1,9 +1,16 @@
-import NavbarAssessor from "../../componentes/NavbarAssessor/NavbarAssessor";
-
-function PlanejadorAssentos() { 
-    return (
-       <NavbarAssessor />
-    );
+import Navbar from "../../componentes/Navbar/Navbar";
+import styles from "./PlanejadorAssentos.module.css";
+function PlanejadorAssentos() {
+  return (
+    <div className={styles.all}>
+      <Navbar />
+      <div className={styles.breve}>
+        <span> Em breve.. </span>
+        <p> Seu evento será ainda mais especial com nosso recurso de planejamento de assentos!  </p>
+        <p> Fique ligado para facilitar a organização do seu grande dia. </p>
+      </div>
+    </div>
+  );
 }
 
 export default PlanejadorAssentos;
