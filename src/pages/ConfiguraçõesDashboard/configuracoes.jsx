ed1c5fd5378307b87522db5840c22e9849163619
Amanda Geovanna <amanda.sousa@sptech.school>
2024-12-08 13:32:39 -0300

    ajustes visuais

--- src/pages/ConfiguraçõesDashboard/configuracoes.jsx
+++ src/pages/ConfiguraçõesDashboard/configuracoes.jsx
@@ -7,76 +7,88 @@ import styles from "./configuracoes.module.css";
 const Configuracoes = () => {
   return (
     <>
-
       <NavComp />
 
       <div className={styles.container}>
         <div className={styles.configuracoesContainer}>
           <div className={styles.colunas}>
-          <span className={styles.tituloPagina}>Configurações da conta</span>
-          <div className={styles.secoesConfiguracoes}>
-
-            <div className={styles.card}>
-              <div className={styles.textosCard}>
-              <div className={styles.cardHeader}>
-                <span className={styles.tituloCard}>Detalhes do casamento</span>
-                <div className={styles.pencil}>
-                <div><FaPen className={styles.iconEdit} /></div></div>
-              
+            <span className={styles.tituloPagina}>Configurações da conta</span>
+            <div className={styles.secoesConfiguracoes}>
+              <div className={styles.card}>
+                <div className={styles.textosCard}>
+                  <div className={styles.cardHeader}>
+                    <span className={styles.tituloCard}>
+                      Detalhes do casamento
+                    </span>
+                    <div className={styles.pencil}>
+                      <div>
+                        <FaPen className={styles.iconEdit} />
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.containerText}>
+                    <div className={styles.cardContent}>
+                      <div className={styles.detailRow}>
+                        <span className={styles.label}>Nome</span>
+                        <span className={styles.value}>Amanda Sousa</span>
+                      </div>
+                      <div className={styles.detailRow}>
+                        <span className={styles.label}>Nome do seu amor</span>
+                        <span className={styles.value}>Enzo Martins</span>
+                      </div>
+                      <div className={styles.detailRow}>
+                        <span className={styles.label}>Data do casamento</span>
+                        <span className={styles.value}>Adicionar</span>
+                      </div>
+                      <div className={styles.detailRow}>
+                        <span className={styles.label}>
+                          Contagem de convidados
+                        </span>
+                        <span className={styles.value}>Adicionar</span>
+                      </div>
+                      <div className={styles.detailRow}>
+                        <span className={styles.label}>Cidade</span>
+                        <span className={styles.value}>São Paulo - SP</span>
+                      </div>
+                      <div className={styles.detailRow}>
+                        <span className={styles.label}>Telefone</span>
+                        <span className={styles.value}>11981819900</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className={styles.containerText}>             
-                 <div className={styles.cardContent}>
-                <div className={styles.detailRow}>
-                  <span className={styles.label}>Nome</span>
-                  <span className={styles.value}>Amanda Sousa</span>
-                </div>
-                <div className={styles.detailRow}>
-                  <span className={styles.label}>Nome do seu amor</span>
-                  <span className={styles.value}>Enzo Martins</span>
-                </div>
-                <div className={styles.detailRow}>
-                  <span className={styles.label}>Data do casamento</span>
-                  <span className={styles.value}>Adicionar</span>
-                </div>
-                <div className={styles.detailRow}>
-                  <span className={styles.label}>Contagem de convidados</span>
-                  <span className={styles.value}>Adicionar</span>
-                </div>
-                <div className={styles.detailRow}>
-                  <span className={styles.label}>Cidade</span>
-                  <span className={styles.value}>São Paulo - SP</span>
-                </div>
-                <div className={styles.detailRow}>
-                  <span className={styles.label}>Telefone</span>
-                  <span className={styles.value}>11981819900</span>
-                </div></div></div>
-              </div>
-            </div>
 
-
-            <div className={styles.card}>
-              <div className={styles.textosCard}>
-              <div className={styles.cardHeader}>
-                <span className={styles.tituloCard2}>Detalhes da conta</span>
-                <div className={styles.pencil2}>
-                <div><FaPen className={styles.iconEdit} /></div></div>
-              
-              </div>
-              <div className={styles.cardContent}>
-                <div className={styles.detailRow}>
-                  <span className={styles.label}>Endereço de email</span>
-                  <span className={styles.value}>amandasbapt@outlook.com</span>
+              <div className={styles.card}>
+                <div className={styles.textosCard}>
+                  <div className={styles.cardHeader}>
+                    <span className={styles.tituloCard2}>
+                      Detalhes da conta
+                    </span>
+                    <div className={styles.pencil}>
+                      <div>
+                        <FaPen className={styles.iconEdit} />
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.cardContent}>
+                    <div className={styles.detailRow}>
+                      <span className={styles.label}>Endereço de email</span>
+                      <span className={styles.value}>
+                        amandasbapt@outlook.com
+                      </span>
+                    </div>
+                    <div className={styles.detailRow}>
+                      <span className={styles.label}>Senha</span>
+                      <span className={styles.value}>*********</span>
+                    </div>
+                  </div>
                 </div>
-                <div className={styles.detailRow}>
-                  <span className={styles.label}>Senha</span>
-                  <span className={styles.value}>*********</span>
-                </div></div>
               </div>
             </div>
           </div>
-          </div></div>
+        </div>
       </div>
-
 
       <Baseboard />
     </>
